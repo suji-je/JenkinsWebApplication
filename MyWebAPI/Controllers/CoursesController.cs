1bc6bf8e467e53d96af98765f239941c7fa0b08c
suji <suji.je@gmail.com>
2018-04-05 06:03:14 +1000

    corrected issues in unit testing

--- MyWebAPI/Controllers/CoursesController.cs
+++ MyWebAPI/Controllers/CoursesController.cs
@@ -15,7 +15,7 @@ namespace MyWebAPI.Controllers
             test.Add(1);
             test.Add(2);
             test.Add(3);
-            test.Add(5 );
+            test.Add(4);
 
             return test;
         }
